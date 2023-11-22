8c8cd036c0ef5684bd12d5483c1ef236cf85c5be
amirali-lll <amirali.dst.lll@gmail.com>
2023-11-22 21:20:49 +0330

    init the core and food app

--- web/basalunch/settings.py
+++ web/basalunch/settings.py
@@ -33,6 +33,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    
+    'food',
+    'core',
+    
 ]
 
 MIDDLEWARE = [

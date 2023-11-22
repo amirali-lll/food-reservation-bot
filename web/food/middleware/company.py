e8e9df50ca42e830c7f2ae380ec4e6e8010c649f
amirali-lll <amirali.dst.lll@gmail.com>
2023-11-23 00:53:12 +0330

    add the company only on api/ endpoints

--- web/food/middleware/company.py
+++ web/food/middleware/company.py
@@ -6,7 +6,9 @@ from django.conf import settings
 
 class CompanyMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        company = request.headers.get('company')
-        if not company:
-            raise Exception('Company header is missing')
-        request.company = company
+        # only do this if url is /api/
+        if request.path.startswith('/api/'):
+            company = request.headers.get('company')
+            if not company:
+                raise Exception('Company header is missing')
+            request.company = company

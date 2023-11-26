90263b0ea0797214dd474c1e7e18dd744c227455
amirali-lll <amirali.dst.lll@gmail.com>
2023-11-26 21:20:59 +0330

    add telegram id to user

--- web/core/models.py
+++ web/core/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from food.models import Participant
+
 
 class User(AbstractUser):
     # participants FK is added to the Participant model
-    def get_participant(self):
-        return self.participants.first()
+    telegram_id = models.PositiveBigIntegerField(null=True,blank=True,unique=True)
+    
+    def get_participant(self,company):
+        if self.participants.first():
+            return self.participants.first()
+        else:
+            participant = Participant.objects.create(user=self,company=company)
+            return participant
         # TODO temporary solution, should be replaced with a more robust one
         # that allows for multiple participants per user
         

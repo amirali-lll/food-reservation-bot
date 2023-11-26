0528c6775fd4265ed206ac8a041342456586c0c7
amirali-lll <amirali.dst.lll@gmail.com>
2023-11-26 21:24:26 +0330

    implement order func to get order data and post it to backend

--- bot/food/order.py
+++ bot/food/order.py
@@ -6,22 +6,23 @@ logger = logging.getLogger(__name__)
 
 
 
-def order(user,meal,meal_id):
-    url = f'{BASE_URL}order/{meal}'
+def order(user,order_type,item_id):
+    url = f'{BASE_URL}orders/order/'
     headers = {
         'Content-Type': 'application/json',
         'company':'BASA',
         }
     body = {
-        'user_id' : user.id,
+        'telegram_id' : user.id,
         'username' : user.username,
         'user_first_name' : user.first_name,
-        'id' : meal_id,
+        'item_id' : item_id,
+        'order_type' : order_type,
         }
     response = requests.post(url, headers=headers,data=json.dumps(body))
     if response.status_code > 201:
         logger.error("Error: API request unsuccessful.\nresponse status code: " + str(response.status_code)+ "\nresponse: " + str(response.text))
         raise Exception("Error: API request unsuccessful.\nresponse status code: " + str(response.status_code)+ "\nresponse: " + str(response.text))
-    logger.info(f"order {meal}:{meal_id} for user {user.id}:{user.username}|{user.first_name} is successful")
+    logger.info(f"order {order_type}:{item_id} for user {user.id}:{user.username}|{user.first_name} is successful")
     logger.info("response: " + str(response.text))
     return response.json()

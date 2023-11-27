7162efbef20be5d74cb730b7801d0c61bf9ff684
amirali-lll <amirali.dst.lll@gmail.com>
2023-11-27 20:11:44 +0330

    add the job queue but it isn't work yet

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,12 @@
-import logging 
+import logging
+from datetime import time
 from telegram import Update
+from telegram.ext import JobQueue
 from bot.application import application 
 from bot.handlers import handlers
+from food.menu import get_menu_text
+
+
 
 
 
@@ -11,6 +16,17 @@ logging.basicConfig(
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
+
+def send_menu(context):
+    chat_id = 1002037948543
+    menu_text = get_menu_text()
+    context.bot.send_message(chat_id=chat_id, text=menu_text)
+
+def setup_queue():
+    job_queue = JobQueue()
+    job_queue.set_dispatcher(application.dispatcher)
+    job_queue.run_daily(send_menu, time(hour=23, minute=57, second=0))
+    job_queue.start()
 
     
 def main() -> None:

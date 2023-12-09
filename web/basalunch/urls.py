78ec68e57a81ab6049618ea17de3842a698f6f3c
amirali-lll <amirali.dst.lll@gmail.com>
2023-12-09 14:46:05 +0330

    add the static urls of media and static to root urls

--- web/basalunch/urls.py
+++ web/basalunch/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,6 @@ urlpatterns = [
     path('api/v1/', include('core.urls')),
     path('api/v1/', include('food.urls')),
     path('', include('core.urls')),
-]
+]\
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
